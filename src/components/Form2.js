892a6f1063159f11d0f4f224b4fdb6b229766217
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-07 13:38:14 -0800

    add state to object/ add textarea

--- src/components/Form2.js
+++ src/components/Form2.js
@@ -1,12 +1,23 @@
 import React from "react";
+import '../style.css'
 
 export default function Form2() {
   const [formData, setFormData] = React.useState({
     firstName: "",
     lastName: "",
+    email: "",
+    comments: "",
   });
+
+  console.log(formData);
+
   function handleChange(event) {
-    console.log(event.target.name);
+    setFormData((prevFormData) => {
+      return {
+        ...prevFormData,
+        [event.target.name]: event.target.value,
+      };
+    });
   }
 
   return (
@@ -14,15 +25,30 @@ export default function Form2() {
       <h3>Form 2 - object</h3>
       <input
         name="firstName"
+        value={formData.firstName}
         type="text"
         placeholder="First Name"
         onChange={handleChange}
       />
       <input
         name="lastName"
+        value={formData.lastName}
         type="text"
         placeholder="Last Name"
         onChange={handleChange}
+      />
+      <input
+        name="email"
+        value={formData.email}
+        type="text"
+        placeholder="Email"
+        onChange={handleChange}
+      />
+      <textarea 
+        value={formData.comments}
+        placeholder="Comments"
+        onChange={handleChange}
+        name="comments"
       />
     </form>
   );
