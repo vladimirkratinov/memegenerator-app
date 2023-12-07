cf1c691c2502d0f6f08c8c3d8c89bfadbb1a86c7
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-12-07 14:10:06 -0800

    add a checkbox to Form2

--- src/components/Form2.js
+++ src/components/Form2.js
@@ -1,5 +1,5 @@
 import React from "react";
-import '../style.css'
+import "../style.css";
 
 export default function Form2() {
   const [formData, setFormData] = React.useState({
@@ -7,15 +7,17 @@ export default function Form2() {
     lastName: "",
     email: "",
     comments: "",
+    isFriendly: false,
   });
 
   console.log(formData);
 
   function handleChange(event) {
+    const { name, value, type, checked } = event.target;
     setFormData((prevFormData) => {
       return {
         ...prevFormData,
-        [event.target.name]: event.target.value,
+        [name]: type === "checkbox" ? checked : value,
       };
     });
   }
@@ -25,31 +27,44 @@ export default function Form2() {
       <h3>Form 2 - object</h3>
       <input
         name="firstName"
-        value={formData.firstName}
         type="text"
         placeholder="First Name"
+        value={formData.firstName}
         onChange={handleChange}
       />
       <input
         name="lastName"
-        value={formData.lastName}
         type="text"
         placeholder="Last Name"
+        value={formData.lastName}
         onChange={handleChange}
       />
       <input
         name="email"
-        value={formData.email}
-        type="text"
+        type="email"
         placeholder="Email"
+        value={formData.email}
         onChange={handleChange}
       />
-      <textarea 
-        value={formData.comments}
-        placeholder="Comments"
-        onChange={handleChange}
+      <textarea
         name="comments"
+        placeholder="Comments"
+        value={formData.comments}
+        onChange={handleChange}
       />
+      <div style={{display: "flex", alignItems: "center", marginLeft: "20px"}}>
+      <input
+        style={{verticalAlign: "middle", margin: 0, marginRight: "5px"}}
+        name="isFriendly"
+        id="isFriendly"
+        type="checkbox"
+        checked={formData.isFriendly}
+        onChange={handleChange}
+      />
+      <label 
+      style={{verticalAlign: "middle", margin: 0}} htmlFor="isFriendly">Are you friendly?</label>
+      <br />
+      </div>
     </form>
   );
 }
